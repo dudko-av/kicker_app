184470ef0f782b1e96959466385216245d7544d8
artem.gulia <artem.gulia@gmail.com>
2016-03-21 10:02:39 +0200

    Add method for getting info about player

--- server/controllers/UsersController.js
+++ server/controllers/UsersController.js
@@ -8,6 +8,15 @@ module.exports.controller = function (app, io) {
         });
     });
 
+    app.post('/users/profile', function(req, res) {
+        if (!authorized(req, res)) return;
+        var User = mongoose.model('User');
+        User.findById(req.body._id, function(err, user) {
+            res.send(user);
+        });
+
+    });
+
     function authorized(req, res) {
         if (!req.user) res.sendStatus(401);
         return req.user ? true : false;
